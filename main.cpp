8f1adea0bcdfd70354182998205605706260c142
Andrew Gonzalez <andrew.gonzal@gmail.com>
2018-11-13 19:02:07 -0800

    Created Quicksort project.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// Andrew Gonzalez
+// CS 350
+// The intent of this project is to measure runtime performance for
+// the Quicksort algorithm using various pivot choosing strategies.
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
