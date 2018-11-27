4d7dd3d0be2feeb4c923911ef7ddad03dd1b4533
Andrew Gonzalez <andrew.gonzal@gmail.com>
2018-11-27 13:37:17 -0800

    Changed quicksort to take command line arguments for which partition algorithm and pivot choice to use.

--- main.cpp
+++ main.cpp
@@ -5,16 +5,27 @@
 
 #include <iostream>
 #include <chrono>
+#include <algorithm>
 #include "quicksort.h"
 
-int main() {
-    using namespace std::chrono;
-    int arrSize = 10000;
-    high_resolution_clock clock;
-    high_resolution_clock::time_point start;
-    high_resolution_clock::time_point end;
-    milliseconds time;
+using namespace std::chrono;
 
+typedef int (*partFunc)(int*,int,int,int);
+typedef int (*pivotFunc)(int*,int,int);
+
+// prototypes
+microseconds startSort(int array[], int arrSize,
+        int (*partitionFunc)(int*,int,int,int), int (*pivotFunc)(int*,int,int));
+partFunc determinePartition(char*);
+pivotFunc determinePivot(char*);
+void printArr(int *pInt, int arrSize);
+
+int main(int argc, char *argv[]) {
+    int (*partitionFunc)(int*,int,int,int);
+    int (*pivotFunc)(int*,int,int);
+    int * sourceArray;
+    int * copyArray;
+    int arrSize = 100;
     // Using a Mersenne twister as the engine to generate random numbers.
     // This will generate uints, and I need ints, so I'm using the
     // uniform_int_distribution engine to transform the uints
@@ -22,21 +33,84 @@ int main() {
     std::random_device seed;
     std::mt19937 randGen(seed());
     std::uniform_int_distribution<int> dist(0, INT32_MAX);
+    // chrono::duration class template using microseconds
+    microseconds timeTaken;
 
-    int * testArr = new int[arrSize];
-    for (int i = 0; i < arrSize; ++i) {
-        testArr[i] = dist(randGen);
-        //std::cout << testArr[i] << std::endl;
+    if (3 == argc) {
+        partitionFunc = determinePartition(argv[1]);
+        pivotFunc = determinePivot(argv[2]);
     }
-    std::cout << "\n\n\nStarting sort..." << std::endl;
-    start = clock.now();
-    quicksort(testArr, 0, arrSize-1, hoarePartition, randomPivot);
-    end = clock.now();
-    time = duration_cast<milliseconds>(end - start);
-    std::cout << "Finished in: " << time.count() << " milliseconds" << std::endl;
-    for (int i = 0; i < arrSize; ++i) {
-        std::cout << testArr[i] << std::endl;
+    else {
+        partitionFunc = lomutoPartition;
+        pivotFunc = basicPivot;
     }
-    delete [] testArr;
+
+    while (arrSize <= 10000) {
+        sourceArray = new int[arrSize];
+        copyArray = new int[arrSize];
+        // Fill source array with random numbers
+        for (int i = 0; i < arrSize; ++i) {
+            sourceArray[i] = dist(randGen);
+        }
+
+        std::cout << "\n\nStarting sort..." << std::endl;
+        for (int i = 0; i < 50; ++i) {
+            std::copy(sourceArray, sourceArray + arrSize, copyArray);
+            timeTaken += startSort(copyArray, arrSize, partitionFunc, pivotFunc);
+        }
+        timeTaken /= 50; // the average of 50 runs of quicksort
+        std::cout << "Finished (size: " << arrSize << "): " << timeTaken.count() << " microseconds" << std::endl;
+        delete[] copyArray;
+        delete[] sourceArray;
+        arrSize += 100;
+    }
     return 0;
 }
+
+
+// Run quicksort using the pivot and partition functions given.
+microseconds startSort(int array[], int arrSize,
+        int (*partitionFunc)(int*,int,int,int), int (*pivotFunc)(int*,int,int)) {
+    high_resolution_clock clock;
+    high_resolution_clock::time_point start;
+    high_resolution_clock::time_point end;
+    microseconds timeTaken;
+
+    start = clock.now();
+    quicksort(array, 0, arrSize - 1, partitionFunc, pivotFunc);
+    end = clock.now();
+    timeTaken += duration_cast<microseconds>(end - start);
+
+    return timeTaken;
+}
+
+
+partFunc determinePartition(char * partition) {
+    if (strcmp("lomuto", partition))
+        return lomutoPartition;
+    else if (strcmp("hoare", partition))
+        return hoarePartition;
+    // default to hoare partition if no match
+    return hoarePartition;
+}
+
+
+pivotFunc determinePivot(char * pivot) {
+    if (strcmp("basic", pivot))
+        return basicPivot;
+    else if (strcmp("random", pivot))
+        return randomPivot;
+    else if (strcmp("m3", pivot))
+        return medianOfThree;
+    else if (strcmp("mM", pivot))
+        return medianOfMedians;
+    // default to basic pivot if no match
+    return basicPivot;
+}
+
+
+void printArr(int *pInt, int arrSize) {
+    for (int i = 0; i < arrSize; ++i)
+        std::cout << pInt[i] << std::endl;
+}
+
